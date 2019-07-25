5d9a8fd9f9f07486a7a5f79e2563d472519687fa
siimplypeachy <48606921+siimplypeachy@users.noreply.github.com>
2019-07-25 14:40:55 -0700

    Add files via upload

---
+++ content/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title></title>
+<script
+src="https://code.jquery.com/jquery-3.3.1.min.js"
+integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="css/style.css"/> 
+<body>
+
+<header class="u-full-width">
+    <div class="column">
+        <div class="eight columns">
+            <a href="#"><img src="imgs/logoleaf.png" alt="logo" width="100"></a>
+        </div>
+
+        <div class="four columns">
+
+            <nav role='navigation'>
+                <div id="menuToggle">
+
+                    <input type="checkbox" />
+                    
+                    <span></span>
+                    <span></span>
+                    <span></span>
+            
+                    <ul id="menu">
+                        <a href=#><img src="#"></a>
+                        <a href="#"><li>Profile  <i class="far fa-user"></i></li></a>
+                        <a href="#"><li>Activity  <i class="fas fa-history"></i></li></a>
+                        <a href="#"><li>Connections  <i class="fas fa-user-friends"></i></li></a>
+                        <a href="#"><li>Logout <i class="fas fa-sign-out-alt"></i></li></a>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
+</header>
